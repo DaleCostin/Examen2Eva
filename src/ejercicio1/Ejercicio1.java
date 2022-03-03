4df227f1f0f57f86bfb4fc631d39aeb14d90a334
DaleCostin <costin.aniwin@gmail.com>
2022-03-03 20:35:26 +0100

    EJERCICIO 1 = (PRE-RELEASE 1.0) EJERCICIO 2 = (PRE-RELEASE 2.0) EJERCICIO 3 = (PRE-RELEASE 1.0)

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -16,6 +16,11 @@ public class Ejercicio1 {
      */
 
 
+    public static int jugadorAleatorio(int maximo) {
+        int random = (int) (Math.random() * (maximo - 1) + 1);
+        return random;
+    }
+
     public static int potenciaDisparo() {
         return (int) (Math.random() * (99 - 50) + 50);
     }
@@ -25,7 +30,7 @@ public class Ejercicio1 {
     }
 
     public static int generarDorsal() {
-        return (int) (Math.random() * 99) + 1 ;
+        return (int) (Math.random() * 99) + 1;
     }
 
     public static String generarJugador() {
@@ -54,13 +59,35 @@ public class Ejercicio1 {
         ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
         creacionJugadores(jugadores);
         System.out.println("ESTOS SON LOS JUGADORES\n");
-        for(int j = 0; j < jugadores.size(); j ++) {
+        for (int j = 0; j < jugadores.size(); j++) {
             System.out.print(jugadores.get(j).getNombre() + "(" + jugadores.get(j).getDorsal() + ")" + " ");
         }
         System.out.println("\n");
         System.out.println("***************************");
         System.out.println("*    COMIENZA EL JUEGO    *");
         System.out.println("***************************\n");
-
+        ArrayList<Jugador> vivos = new ArrayList<Jugador>();
+        vivos = jugadores;
+        int np = 0;
+        while (vivos.size() != 1 && !vivos.isEmpty()) {
+            int jvivos = vivos.size();
+            Jugador portero = vivos.get(jugadorAleatorio(jvivos));
+            Jugador disparador = vivos.get(jugadorAleatorio(jvivos));
+            if (portero.getCalidad() < disparador.getPotenca()) {
+                if (portero.getVidas() > 0) {
+                    portero.setVidas(portero.getVidas() - 1);
+                } else {
+                    vivos.remove(portero);
+                }
+            } else {
+                if (disparador.getVidas() > 0) {
+                    disparador.setVidas(disparador.getVidas() - 1);
+                } else {
+                    vivos.remove(disparador);
+                }
+            }
+        }
+            System.out.println("EL GANADOR ES: ");
+            System.out.println(vivos.get(0));
     }
 }
