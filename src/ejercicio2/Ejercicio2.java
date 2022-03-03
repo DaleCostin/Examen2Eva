a5eee73263a1d742147bfe2292c7e0dea3df5605
DaleCostin <costin.aniwin@gmail.com>
2022-03-03 17:49:46 +0100

    SEGUNDO COMMIT

--- src/ejercicio2/Ejercicio2.java
+++ src/ejercicio2/Ejercicio2.java
@@ -13,12 +13,47 @@ public class Ejercicio2 {
      * indicando el número de palabras del texto cuya longitud es mayor
      * o igual que [longitud].
      */
-    public static int palabrasMasLargasQue(int longitud) {
+    public static int palabrasMasLargasQue(int longitud, String ruta) {
+        File file = new File(ruta);
+        int contador = 0;
 
+        try {
+
+            Scanner f = new Scanner(file);
+
+            while (f.hasNextLine()) {
+                String line = f.nextLine();
+                line = line
+                        .replaceAll("\\.", "").replaceAll(":", "")
+                        .replaceAll("\"", "").replaceAll(",", "")
+                        .replaceAll("\\)", "").replaceAll("\\(", "");
+                String[] lineSp = line.split(" ");
+                for (int i = 0; i < lineSp.length; i++) {
+                    if (lineSp[i] != " ") {
+                        System.out.println(lineSp[i]);
+                        if (lineSp[i].length() > longitud) {
+                            contador++;
+                        }
+                    }
+                }
+                System.out.println(contador);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("RUTA DE FICHERO ERRONEA");
+        }
         return 0;
     }
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("INGRESA RUTA COMPLETA DEL ARCHIVO TXT: ");
+        String ruta = input.nextLine();
+        System.out.println("INGRESAR LONGITUD A COMPARAR");
+        try {
+            int longitud = input.nextInt();
+            palabrasMasLargasQue(longitud, ruta);
+        } catch (Exception e) {
+            System.out.println("ERROR NUMERO NO VALIDO");
+        }
     }
 }
