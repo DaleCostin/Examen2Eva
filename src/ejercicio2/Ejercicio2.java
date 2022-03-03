8b3accfeb1e4263e2d17381c4229c3166ceb7ff6
DaleCostin <costin.aniwin@gmail.com>
2022-03-03 17:17:18 +0100

    PRIMER COMMIT

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,24 @@
+package ejercicio2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    /**
+     * Pre: ---
+     * Post: Dado el fichero “textoExamen.txt”, implementa un método
+     * palabrasMasLargasQue(int longitud) el cual devuelve un entero
+     * indicando el número de palabras del texto cuya longitud es mayor
+     * o igual que [longitud].
+     */
+    public static int palabrasMasLargasQue(int longitud) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
